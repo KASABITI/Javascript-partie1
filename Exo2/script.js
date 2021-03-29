c6b0b4a1226df30ec9d4180d72c931c5467c55ff
KASABITI <aggieksbt@gmail.com>
2021-03-29 23:12:48 +0200

    Mise ajour exo2

--- Exo2/script.js
+++ Exo2/script.js
@@ -2,6 +2,6 @@ var nom = 'Kasabiti';
 var prenom = 'Agnes';
 var ville = 'Aire sur l\'Adour ';
 
-var res = 'Nom :' + nom + \n + 'Prenom :' + prenom + \n  + 'Ville :' + ville;
+var res = 'Nom :' + nom +  '\n' + 'Prenom :' + prenom + '\n'  + 'Ville :' + ville;
 
 alert(res);
